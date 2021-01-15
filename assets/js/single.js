61524f734adebf4dd29e43eafc76535f259f9ab1
J Scott Rumptz <jscott@rumptz.tech>
2021-01-15 13:57:23 -0600

    over 30 issue warning

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 let issueContainerEl = document.querySelector("#issues-container");
+let limitWarningEl = document.querySelector("#limit-warning");
+
 
 let getRepoIssues = function(repo) {
     let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,9 +11,13 @@ let getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                if(response.headers.get("Link")) {
+                    displayWarning();
+                }
             })
         } else {
-            alert("THere was a promblem with your request!")
+            alert("There was a promblem with your request!")
         }
     })
 };
@@ -54,5 +60,18 @@ let displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+let displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    let linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
 
 getRepoIssues("facebook/react");
